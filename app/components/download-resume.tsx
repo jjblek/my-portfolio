b9d6226216d802161cd84ae508c5fb7b658e95ce
jjblek <jjblek@gmail.com>
2025-09-02 16:38:05 -0700

    resume size updated

--- app/components/download-resume.tsx
+++ app/components/download-resume.tsx
@@ -13,7 +13,7 @@ export default function DownloadResume() {
                 className="absolute text-xs uppercase scale-0 rounded-md py-2 px-2 bg-neutral-300 dark:bg-neutral-700 left-2/4 mb-3 bottom-full group-hover/download:scale-100 origin-bottom transition-all duration-300 shadow-lg 
                 before:content-[''] before:absolute before:top-full before:left-2/4 before:w-3 before:h-3 before:border-solid before:bg-neutral-300 before:dark:bg-neutral-700 before:rotate-45 before:-translate-y-2/4 before:-translate-x-2/4"
             >
-                64KB
+                214KB
             </div>
             </button>
         </a>
